767782973764bd3f3e73aa7430663b19164e428e
Barnabás Baka <barnabas@twentythree.com>
2024-04-29 10:00:06 +0200

    moving js to the script file and style improvements

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 let number = "3";
 console.log(number);
+
+let counterValue = 0;
+function updateCounterDisplay() {
+  document.getElementById("counter").textContent = counterValue;
+}
+function increaseCounter() {
+  counterValue++;
+  updateCounterDisplay();
+}
+function decreaseCounter() {
+  counterValue--;
+  updateCounterDisplay();
+}
+function resetCounter() {
+  counterValue = 0;
+  updateCounterDisplay();
+}
